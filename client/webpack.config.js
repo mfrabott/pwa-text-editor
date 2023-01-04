91bb92ac31201b772f6973051ada7ef324b57602
mfrabott <mfrabott14@gmail.com>
2023-01-04 17:15:21 -0500

    setup webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,17 +13,48 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
+
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+
+      new MiniCssExtractPlugin(),
+      
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
