762d284931d7104b3cddab5f06540fbad1643706
mfrabott <mfrabott14@gmail.com>
2023-01-04 18:05:19 -0500

    set up manifest creation

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -15,7 +17,7 @@ module.exports = () => {
     },
     
     output: {
-      filename: 'bundle.js',
+      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
 
@@ -28,11 +30,23 @@ module.exports = () => {
 
       new MiniCssExtractPlugin(),
       
-      new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Keep notes online and offline!',
+        // background_color: '#7eb4e2',
+        // theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
-      
     ],
 
     module: {
