c29a4014ec6fda97f05293fdfcc2b16841b18482
mfrabott <mfrabott14@gmail.com>
2023-01-05 20:29:25 -0500

    fix getdb bug

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,7 +26,11 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      if (data) {
+        this.editor.setValue(data);
+      } else {
+        this.editor.setValue(localData || header);
+      }
     });
 
     this.editor.on('change', () => {
